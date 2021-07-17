463bdb9e2ea0c5ee0a4ab30a8bb0b48de696a4c9
Diogo <teabr@hotmail.com>
2021-07-17 00:03:00 -0300

    First commit

---
+++ src/main/kotlin/lessmann/betina/EnglishClass/EnglishClassApplication.kt
@@ -0,0 +1,11 @@
+package lessmann.betina.EnglishClass
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EnglishClassApplication
+
+fun main(args: Array<String>) {
+	runApplication<EnglishClassApplication>(*args)
+}
---
+++ src/test/kotlin/lessmann/betina/EnglishClass/EnglishClassApplicationTests.kt
@@ -0,0 +1,13 @@
+package lessmann.betina.EnglishClass
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EnglishClassApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
