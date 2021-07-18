ff451625cd228b0de00847b61f10a8148747693b
Diogo <teabr@hotmail.com>
2021-07-18 18:28:38 -0300

    Update ScheduleController.java

--- src/main/java/com/lessmann/betina/TeacherBetina/controllers/ScheduleController.java
+++ src/main/java/com/lessmann/betina/TeacherBetina/controllers/ScheduleController.java
@@ -1,13 +1,9 @@
 package com.lessmann.betina.TeacherBetina.controllers;
 
 import com.lessmann.betina.TeacherBetina.entities.Schedule;
-import com.lessmann.betina.TeacherBetina.entities.Teacher;
 import com.lessmann.betina.TeacherBetina.repositories.ScheduleRepository;
-import com.lessmann.betina.TeacherBetina.repositories.TeacherRepository;
 import com.lessmann.betina.TeacherBetina.repositories.dao.ScheduleDAO;
-import com.lessmann.betina.TeacherBetina.repositories.dao.TeacherDAO;
 import com.lessmann.betina.TeacherBetina.services.ScheduleService;
-import com.lessmann.betina.TeacherBetina.services.TeacherService;
 import com.lessmann.betina.TeacherBetina.standard.StandardController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
