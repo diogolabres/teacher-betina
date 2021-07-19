c067dd9ee2a91cabf3cb8b43abec388357cd69b7
Diogo <teabr@hotmail.com>
2021-07-19 13:17:57 -0300

    Update RestExceptionHandler.java

--- src/main/java/com/lessmann/betina/TeacherBetina/handlers/RestExceptionHandler.java
+++ src/main/java/com/lessmann/betina/TeacherBetina/handlers/RestExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.lessmann.betina.TeacherBetina.handlers;
 
 import com.lessmann.betina.TeacherBetina.errors.RuntimeExceptionDetails;
+import com.lessmann.betina.TeacherBetina.standard.exceptions.ImpossibleDeleteException;
+import com.lessmann.betina.TeacherBetina.standard.exceptions.RecordNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -12,14 +14,38 @@ import java.time.LocalDateTime;
 public class RestExceptionHandler {
 
     @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<RuntimeExceptionDetails> handlerRuntimeException(RuntimeException runtimeException){
+    public ResponseEntity<RuntimeExceptionDetails> handlerRuntimeException(RuntimeException exception){
         return new ResponseEntity<>(
                 RuntimeExceptionDetails.builder()
                         .timestamp(LocalDateTime.now())
                         .status(HttpStatus.BAD_REQUEST.value())
                         .title("Bad Request Exception, Check the Documentation")
-                        .details(runtimeException.getMessage())
-                        .developerMessage(runtimeException.getClass().getName())
+                        .details(exception.getMessage())
+                        .developerMessage(exception.getClass().getName())
                         .build(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ImpossibleDeleteException.class)
+    public ResponseEntity<RuntimeExceptionDetails> handlerImpossibleDeleteException(ImpossibleDeleteException exception){
+        return new ResponseEntity<>(
+                RuntimeExceptionDetails.builder()
+                        .timestamp(LocalDateTime.now())
+                        .status(HttpStatus.NOT_FOUND.value())
+                        .title("Bad Request Exception, Check the Documentation")
+                        .details(exception.getMessage())
+                        .developerMessage(exception.getClass().getName())
+                        .build(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(RecordNotFoundException.class)
+    public ResponseEntity<RuntimeExceptionDetails> handlerRecordNotFoundException(RecordNotFoundException exception){
+        return new ResponseEntity<>(
+                RuntimeExceptionDetails.builder()
+                        .timestamp(LocalDateTime.now())
+                        .status(HttpStatus.NOT_FOUND.value())
+                        .title("Bad Request Exception, Check the Documentation")
+                        .details(exception.getMessage())
+                        .developerMessage(exception.getClass().getName())
+                        .build(), HttpStatus.NOT_FOUND);
     }
 }
